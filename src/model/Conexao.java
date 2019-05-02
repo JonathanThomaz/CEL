855f772a982b42cc8a84ae9cbc12d6ac74fb2f32
Jonathan <jonathanthomaz96@gmail.com>
2019-05-01 21:53:15 -0300

    create method enviar

--- src/model/Conexao.java
+++ src/model/Conexao.java
@@ -5,6 +5,16 @@
  */
 package model;
 
+import java.util.Properties;
+import javax.mail.Address;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
 /**
  *
  * @author admin
@@ -14,6 +24,15 @@ public class Conexao {
     private String porta;
     private String smtpServer;
     private String smtpPort;
+    private Email email;
+
+    public Email getEmail() {
+        return email;
+    }
+
+    public void setEmail(Email email) {
+        this.email = email;
+    }
 
     public String getServidor() {
         return servidor;
@@ -47,6 +66,49 @@ public class Conexao {
         this.smtpPort = smtpPort;
     }
     
+    
+    public void enviarEmail(){
+        Properties props = new Properties();
+            /** Parâmetros de conexão com servidor */
+            props.put("mail.smtp.host", smtpServer);
+            props.put("mail.smtp.socketFactory.port", smtpPort);
+            props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+            props.put("mail.smtp.auth", "true");
+            props.put("mail.smtp.port", smtpPort);
+
+        Session session;
+        session = Session.getDefaultInstance(props,
+        new javax.mail.Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication()
+               {
+                    return new PasswordAuthentication(email.getRemetente(), email.getAutenticacao());
+               }
+            });
+ 
+        /** Ativa Debug para sessão */
+        session.setDebug(true);
+ 
+        try {
+ 
+                Message message = new MimeMessage(session);
+                message.setFrom(new InternetAddress(email.getRemetente())); //Remetente
+ 
+                  Address[] toUser = InternetAddress //Destinatário(s)
+                             .parse(email.getRemetente());  
+ 
+                  message.setRecipients(Message.RecipientType.TO, toUser);
+                  message.setSubject(email.getAssunto());//Assunto
+                  message.setText(email.getCorpo());
+                  /**Método para enviar a mensagem criada*/
+                  Transport.send(message);
+ 
+                  System.out.println("Feito!!!");
+ 
+             } catch (MessagingException e) {
+                  throw new RuntimeException(e);
+            }
+    }
     
     
 }
